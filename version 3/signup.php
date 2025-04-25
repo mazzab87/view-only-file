48fc77174f76a6dd1154b544e62100efee9f83ea
mazzab87 <156071757+mazzab87@users.noreply.github.com>
2025-04-25 11:15:13 +0100

    Add files via upload

--- version 3/signup.php
+++ version 3/signup.php
@@ -1,24 +1,33 @@
 <?php 
 require ('db.php');
-if ($_SERVER["REQUEST_METHOD"] == "POST") { 
-     $first_name = $_POST["first_name"]; 
-     $last_name = $_POST["last_name"];  
-     $email = $_POST["email"];
-     $password = md5($_POST["password"]);
-     $password = md5($password);
-      
-     
-     $query = "INSERT INTO 'users' (first_name, last_name, email, password) VALUES ($first_name, $last_name, $email, $password)"; 
-     $result = mysqli_query($conn, $query);
-     if ($result) {
-        echo "Registration successful";
-     }
-     else {
-        echo("Registration failed");
-     }
 
+
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") { 
+    
+    if (!isset($_POST["first_name"]) || !isset($_POST["last_name"]) || !isset($_POST["email"]) || !isset($_POST["password"])) {
+        die("All fields are required");
+    }
+
+    // This part sanitizes the input
+    $first_name = mysqli_real_escape_string($conn, $_POST["first_name"]); 
+    $last_name = mysqli_real_escape_string($conn, $_POST["last_name"]);  
+    $email = mysqli_real_escape_string($conn, $_POST["email"]);
+    $password = md5($_POST["password"]); // Single MD5 hash is sufficient
+      
+    
+    $query = "INSERT INTO users (first_name, last_name, email, password) VALUES ('$first_name', '$last_name', '$email', '$password')"; 
+    
+    // Execute query and handle errors
+    $result = mysqli_query($conn, $query);
+    
+    if ($result) {
+        echo "Registration successful";
+    } else {
+        echo "Registration failed: " . mysqli_error($conn);
+    }
 }
 ?>
 
-
- 
